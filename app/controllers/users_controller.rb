27d9c3e3c931db8418b8762196f36af3234919da
Irfan Maknojia <imak_7@yahoo.com>
2019-10-07 16:11:44 -0500

    update create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,13 @@ class UsersController < ApplicationController
     end
 
     def create
+        @user = User.new(user_params)
 
+        if @user.save
+            #session[user_id] = @user.id
+            render json: @user
+        else
+            render json: {status 500, message: 'User cannot be created'}
     end
 
     def update
