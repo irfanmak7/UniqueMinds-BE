bd4f6d0a80ca5f48702aebbda68203ef1dc7d9b0
Irfan Maknojia <imak_7@yahoo.com>
2019-10-07 16:28:08 -0500

    create index, show, create, update and destroy methods

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,3 +1,23 @@
 class QuotesController < ApplicationController
 
+    def index
+
+    end
+
+    def show
+        
+    end
+
+    def create
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
 end
