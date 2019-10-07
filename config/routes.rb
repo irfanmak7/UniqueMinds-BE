5d0c5b2f7673cc38485fc4c78b39373234812437
Irfan Maknojia <imak_7@yahoo.com>
2019-10-07 17:13:12 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+      resources :users
+      resources :quotes
+    end
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users
-  resources :quotes
 end
